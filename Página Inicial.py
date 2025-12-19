34873c35190647309cdcce741c6b312f7fd88987
KrlosAguiar <c.aguiar.junior@gmail.com>
2025-12-19 01:40:51 -0300

    Update Página Inicial.py

--- Página Inicial.py
+++ Página Inicial.py
@@ -18,21 +18,22 @@ st.markdown("---")
 
 st.markdown("""
 ### Bem-vindo ao sistema centralizado de ferramentas contábeis.
-
+<br>
 Utilize o menu lateral à esquerda para navegar entre os módulos disponíveis:
-
+<br>
 - **Conciliador Bancário:** Cruza os dados do Extrato Bancário com o Razão da Contabilidade.
 
 - **Tarifas Bancárias:** Extrai os lançamentos das tarifas do Extrato Bancário e emite um relatório pronto para empenho, liquidação e pagamento.
 
 - **(Em breve) Novos Módulos:** Outras ferramentas serão adicionadas aqui.
-
+<br>
 ---
 **Status do Sistema:** ✅ Online
 """)
 
 # Dica: Se quiser que o login seja feito AQUI e valha para tudo,
 # você pode mover a função check_password para cá no futuro.
+
 
 
 
