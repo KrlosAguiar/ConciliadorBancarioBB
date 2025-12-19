2bdf042b1608748a824ca3a2e4eb08ca173096ed
KrlosAguiar <c.aguiar.junior@gmail.com>
2025-12-19 00:53:32 -0300

    Update Página Inicial.py

--- Página Inicial.py
+++ Página Inicial.py
@@ -8,21 +8,23 @@ icon_path = os.path.join(os.getcwd(), "Barcarena.png")
 icon_image = Image.open(icon_path)
 
 st.set_page_config(
-    page_title="Decon - Barcarena",
+    page_title="DECON - Barcarena/PA",
     page_icon=icon_image, # Aqui aplicamos o seu ícone customizado
     layout="wide"
 )
 
-st.title("Portal de Ferramentas Contábeis")
+st.title("Departamento de Contabilidade - Barcarena/PA")
 st.markdown("---")
 
 st.markdown("""
-### Bem-vindo ao sistema centralizado.
+### Bem-vindo ao sistema centralizado de ferramentas contábeis.
 
 Utilize o menu lateral à esquerda para navegar entre os módulos disponíveis:
 
 - **Conciliador Bancário:** Cruza os dados do Extrato Bancário com o Razão da Contabilidade.
+
 - **Tarifas Bancárias:** Extrai os lançamentos das tarifas do Extrato Bancário e emite um relatório pronto para empenho, liquidação e pagamento.
+
 - **(Em breve) Novos Módulos:** Outras ferramentas serão adicionadas aqui.
 
 ---
@@ -31,6 +33,7 @@ Utilize o menu lateral à esquerda para navegar entre os módulos disponíveis:
 
 # Dica: Se quiser que o login seja feito AQUI e valha para tudo,
 # você pode mover a função check_password para cá no futuro.
+
 
 
 
