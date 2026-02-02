a9b2f2fcd2bcaa3a1a13da8a3d3064f0af397bb1
KrlosAguiar <c.aguiar.junior@gmail.com>
2026-02-02 15:02:27 -0300

    Update Página Inicial.py

--- Página Inicial.py
+++ Página Inicial.py
@@ -58,6 +58,8 @@ Utilize o menu lateral à esquerda para navegar entre os módulos disponíveis:
 
 * **Projeção de FOPAG:** Calcula a projeção da Folha de Pagamento para os meses restantes.
 
+* **Conferência Geral:** Realiza conferências de rotinas mensais.
+
 * **Tarifas Bancárias:** Extrai os lançamentos das tarifas do Extrato Bancário para empenho, liquidação e pagamento.
 
 * **(Em breve) Novos Módulos:** Outras ferramentas serão adicionadas aqui.
@@ -68,6 +70,7 @@ Utilize o menu lateral à esquerda para navegar entre os módulos disponíveis:
 ---
 **Status do Sistema:** ✅ Online
 """)
+
 
 
 
